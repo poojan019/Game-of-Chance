640ca8420b287bee0417e134863a5b78b8faf2d2
poojan019 <57913568+poojan019@users.noreply.github.com>
2024-06-25 14:16:16 +0530

    Add files via upload

---
+++ errorHeader.h
@@ -0,0 +1,17 @@
+// A function to display an error message and the exit
+void fatal(char *message){
+    char error_message[100];
+    strcpy(error_message, "[!!] Fatal Error");
+    strncat(error_message, message, 83);
+    perror(error_message);
+    exit(-1);
+}
+
+// An error-checked malloc() wrapper function
+void *ec_mallod(unsigned int size){
+    void *ptr;
+    ptr = malloc(size);
+    if (ptr == NULL)
+        fatal("in ec_malloc() on memory allocation");
+    return ptr;
+}
